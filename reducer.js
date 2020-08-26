fc77f7711ac34ef0c7c136eef3602d9e89d02928
Charles Elloit <62748531+CharlesElloit@users.noreply.github.com>
2020-08-26 07:53:54 -0400

    Update reducer.js

--- reducer.js
+++ reducer.js
@@ -1,12 +1,27 @@
+/* 
+ Am using ES5 cuz am going to run this code using Node
+ but you can use ES6 import snytax in your application
+*/
+
 const userReducer = require("./userReducer");
 const contactReducer = require("./contactReducer");
 const productReducer = require("./productReducer");
 
-//CombineReducer
+// import {combineReducer} from 'redux'
+
+//Redux combineReducer
+// const reduxCombineReducer = combineReducer({
+//   users: userReducer,
+//   contacts: contactReducer,
+//   products: productReducer,
+// })
+
+//Custom CombineReducer
 const combineReducer = (prevState, action) => ({
   users: userReducer(prevState.users, action),
   contacts: contactReducer(prevState.contacts, action),
   products: productReducer(prevState.products, action)
 });
 
+// exports default reduxCombinReducer as combineReducer;
 module.exports = combineReducer;
